24099811a090c6b90460703c6767bf9f20cba7bf
programer10 <programer10@ucione.local>
2019-02-26 16:12:57 +0100

    dodona vjezba_5.2.3_veliki_broj

---
+++ ConsoleApp1/vjezba_5.2.3_veliki_broj/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba_5._2._3_veliki_broj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Upišite broj: ");
+            int broj = int.Parse(Console.ReadLine());
+
+            if (broj >= 100)
+            {
+                Console.WriteLine("Broje je velik!");
+            }
+            else
+            {
+                Console.WriteLine("Broj NIJE velik!");
+            }
+            Console.ReadKey();
+        }
+    }
+}

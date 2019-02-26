0eeb0ed0510225cec1a63a260f6472c76aa08894
rihard11 <rihardla@gmail.com>
2019-02-26 12:57:41 +0100

    dodan primjer_5.2.5_paran_neparan

---
+++ ConsoleApp1/primjer_5.2.5_paran_neparan/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primjer_5._2._5_paran_neparan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj: ");
+            int broj = int.Parse(Console.ReadLine());
+
+            if (broj % 2 == 0)
+            {
+                Console.WriteLine("Broje je paran!");
+            }
+            else
+            {
+                Console.WriteLine("Broja je neparan!");
+            }
+            Console.ReadKey();
+        }
+    }
+}

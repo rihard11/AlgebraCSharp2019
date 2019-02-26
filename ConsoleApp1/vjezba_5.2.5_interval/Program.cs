1bb2bb10933606354bd3c3a2c79edaf759ae24f5
programer10 <programer10@ucione.local>
2019-02-26 16:37:06 +0100

    dodana vjezba_5.2.5

---
+++ ConsoleApp1/vjezba_5.2.5_interval/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba_5._2._5_interval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj: ");
+            int broj = int.Parse(Console.ReadLine());
+
+            if (broj >= 100 && broj <= 200)
+            {
+                Console.WriteLine("Broj je u intervalu");
+            }
+            else
+            {
+                Console.WriteLine("Broj nije u intervalu!");
+            }
+            Console.ReadKey();
+        }
+    }
+}

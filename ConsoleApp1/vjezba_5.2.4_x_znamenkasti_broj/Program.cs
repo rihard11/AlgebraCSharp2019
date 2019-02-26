b74557f644ce826b54420411576ec3c686316598
programer10 <programer10@ucione.local>
2019-02-26 16:27:42 +0100

    dodana vjezba_5.2.4

---
+++ ConsoleApp1/vjezba_5.2.4_x_znamenkasti_broj/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba_5._2._4_x_znamenkasti_broj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj: ");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a < 0)
+            {
+                a = -a;
+            }
+
+            if (a < 10)
+            {
+                Console.WriteLine("Broj je jednoznamenkast");
+            }
+            else if (a < 100)
+            {
+                Console.WriteLine("Broj je dvoznamenkast");
+            }
+            else if (a  < 1000)
+            {
+                Console.WriteLine("Broje je troznamenkast");
+            }
+            else
+            {
+                Console.WriteLine("Broj je višeznamenkast");
+            }
+            Console.ReadLine();
+        }
+    }
+}

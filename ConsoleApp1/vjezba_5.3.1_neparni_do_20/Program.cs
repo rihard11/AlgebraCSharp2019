2a2ac97a13ee61df88e70572d02c3a40f193fd1c
programer10 <programer10@ucione.local>
2019-02-26 18:22:03 +0100

    vjezba_5.3.1_neparni_do_20

---
+++ ConsoleApp1/vjezba_5.3.1_neparni_do_20/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba_5._3._1_neparni_do_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}

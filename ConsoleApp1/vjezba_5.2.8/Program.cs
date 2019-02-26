95c445f766dc0e1c50ddcaef4e495f3937a9cd02
programer10 <programer10@ucione.local>
2019-02-26 16:59:45 +0100

    dodana vjezba_5.2.8

---
+++ ConsoleApp1/vjezba_5.2.8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba_5._2._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite godinu: ");
+            int godina = int.Parse(Console.ReadLine());
+
+            if ((godina % 4 == 0 && (!(godina % 100 == 0))) || (godina % 400 == 0))
+            {
+                Console.WriteLine("Godina je prijestupna");
+            } else
+            {
+                Console.WriteLine("Godina nije prijestupna");
+            }
+            Console.ReadKey();
+        }
+    }
+}

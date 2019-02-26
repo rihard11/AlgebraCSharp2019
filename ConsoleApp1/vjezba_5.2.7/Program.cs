aa5f15b8a6f3eb69d6e1b31e5982d718e83b75cb
programer10 <programer10@ucione.local>
2019-02-26 16:49:26 +0100

    dodana vjezba_5.2.7

---
+++ ConsoleApp1/vjezba_5.2.7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba_5._2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj: ");
+            int broj = int.Parse(Console.ReadLine());
+
+            if (broj % 4 == 0 && broj >= 100)
+            {
+                Console.WriteLine("Broj je dobar!");
+            }
+            else
+            {
+                Console.WriteLine("Broj nije dobar");
+            }
+            Console.ReadKey();
+        }
+    }
+}

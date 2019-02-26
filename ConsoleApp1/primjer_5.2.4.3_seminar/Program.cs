2c5c960a89046842255fd3e1d9fc62ceb15ef21b
rihard11 <rihardla@gmail.com>
2019-02-26 12:30:06 +0100

    dodan primjer_5.2.4.3_seminar

---
+++ ConsoleApp1/primjer_5.2.4.3_seminar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primjer_5._2._4._3_seminar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("unesite ime škole koju pohađate: ");
+            string skola = Console.ReadLine();
+
+            if (skola != "Algebra")
+            {
+                Console.WriteLine("Upišite informatički seminar u Algebri!");
+            }
+            else
+            {
+                Console.WriteLine("Bravo!");
+            }
+            Console.ReadKey();
+        }
+    }
+}

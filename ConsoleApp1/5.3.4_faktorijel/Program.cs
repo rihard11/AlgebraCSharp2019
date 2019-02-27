4d7b30d81e1cd6bb210cc7600e5e447cdb861d5b
programer10 <programer10@ucione.local>
2019-02-27 16:50:57 +0100

    factorijal sa Exception

--- ConsoleApp1/5.3.4_faktorijel/Program.cs
+++ ConsoleApp1/5.3.4_faktorijel/Program.cs
@@ -10,17 +10,88 @@ namespace _5._3._4_faktorijel
     {
         static void Main(string[] args)
         {
+            //Console.Write("Unesite broj: ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //long fakt = 1;
+
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    fakt *= i;
+            //}
+            //Console.WriteLine("{0}! = {1}", a, fakt);
+            //Console.ReadKey();   
+
             Console.Write("Unesite broj: ");
-            int a = int.Parse(Console.ReadLine());
-
-            long fakt = 1;
-
-            for (int i = 1; i <= a; i++)
+            try
             {
-                fakt *= i;
+                int a = int.Parse(Console.ReadLine());
+                if (a <= 0)
+                {
+                    throw new NegativeNumberException("Negativni broj. Unesite pozitivni broj");
+                }
+                if (a >= 20)
+                {
+                    throw new Exception("Broj je prevelik, rezultat je veći ok dimenzije 'int' 2.147.483.647");
+                }
+
+                int fact = 1;
+                for (int i = a; i > 0; i--)
+                {
+                    //Console.WriteLine(i);
+                    fact *= i;
+                }
+                Console.WriteLine(fact);
             }
-            Console.WriteLine("{0}! = {1}", a, fakt);
-            Console.ReadKey();   
+            catch (NegativeNumberException ex)
+            {
+                Console.WriteLine("Negativni broj" + ex.ToString());
+            }
+            catch (VeciOdDvajstException ex)
+            {
+                Console.WriteLine("Dozvoljeni brojevi su u intervalu 1 do 19" + ex.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+            Console.ReadKey();
+            }
+        }
+        public class NegativeNumberException : Exception
+        {
+            public NegativeNumberException()
+            {
+            }
+
+            public NegativeNumberException(string message)
+            : base(message)
+            {
+            Console.WriteLine("Dodatno custom pojašnjenje klase NegativNumberException");
+            }
+
+            public NegativeNumberException(string message, Exception inner)
+            : base(message, inner)
+            {
+            }
+        }
+        public class VeciOdDvajstException : Exception
+        {
+            public VeciOdDvajstException()
+            {
+            }
+
+            public VeciOdDvajstException(string message)
+            : base(message)
+            {
+            }
+
+            public VeciOdDvajstException(string message, Exception inner)
+            : base(message, inner)
+            {
+            }
         }
     }
-}
+

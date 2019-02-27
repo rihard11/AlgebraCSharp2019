53f3d84d2ca937e285eeee32d8afb5c26afef060
programer10 <programer10@ucione.local>
2019-02-27 17:37:37 +0100

    dodoan 5.3.9_djelitelj

---
+++ ConsoleApp1/5.3.9_djelitelj/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._9_djelitelj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite prirodni broj: ");
+            int broj = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < broj; i++)
+            {
+                if (broj % i == 0)
+                {
+                    Console.WriteLine("{0} ", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}

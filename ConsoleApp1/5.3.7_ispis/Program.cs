18176907ef1ebb17c669c57106fbbe231523adde
programer10 <programer10@ucione.local>
2019-02-27 17:28:19 +0100

    dodan 5.3.7_ispis

---
+++ ConsoleApp1/5.3.7_ispis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._7_ispis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if ((i % 7 == 0) && (!(i % 3 == 0)))
+                {
+                    Console.WriteLine("{0} ", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}

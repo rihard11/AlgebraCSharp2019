9e51053daba09597015e54aed987acd42b2c6358
rihard11 <rihardla@gmail.com>
2019-02-27 13:39:02 +0100

    Vjezbe_Kuca dodan 5.3.5_djeljiv_s3

---
+++ ConsoleApp1/5.3.5_djeljivi_s3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._5_djeljivi_s3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Brojevi iz intervala od 1 do 100 djeljivi sa 3 su: ");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}

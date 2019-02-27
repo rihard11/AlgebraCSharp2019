b3b23595ab0fcb57319545c3bfabb19a51ea49a8
programer10 <programer10@ucione.local>
2019-02-27 17:58:26 +0100

    dodan 5.3.11

---
+++ ConsoleApp1/5.3.11_savrseni_brojevi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._11_savrseni_brojevi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Savršeni brojevi iz intervala 1 do 1000 su: ");
+            int broj;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                broj = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        broj += j;
+                    }
+                }
+                if (i == broj)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}

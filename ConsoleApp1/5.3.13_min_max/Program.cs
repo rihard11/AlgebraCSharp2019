adb06fa3414f5d95e76d4e3169ee1c0b7a876ff6
programer10 <programer10@ucione.local>
2019-02-27 18:19:49 +0100

    5.3.13_min_max

---
+++ ConsoleApp1/5.3.13_min_max/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._13_min_max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = int.MinValue;
+            int max = int.MaxValue;
+            int b;
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Unesite {0}. prirodni broj", i);
+                b = int.Parse(Console.ReadLine());
+
+                if (b < min)
+                {
+                    min = b;
+                }
+                if (b > max)
+                {
+                    max = b;
+                }
+            }
+            Console.WriteLine("Najmanji: {0}\n Najveći: {1}", min, max);
+            Console.ReadKey();
+        }
+    }
+}

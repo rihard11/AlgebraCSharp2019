0ce31372eb4c19b515b1d16bdfbbcb7483caed57
rihard11 <rihardla@gmail.com>
2019-02-28 20:19:13 +0100

    dodan 5.3.15_ispis_unatrag

---
+++ ConsoleApp1/5.3.15_ispis_unatrag/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._15_ispis_unatrag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ispis brojeva od 10 do 1
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}

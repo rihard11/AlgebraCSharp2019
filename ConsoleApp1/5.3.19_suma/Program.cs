f822692810f2a4d1ef0efa17eb04a9ddd78617f1
rihard11 <rihardla@gmail.com>
2019-02-28 20:53:54 +0100

    dodan 5.3.19_suma

---
+++ ConsoleApp1/5.3.19_suma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._19_suma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            int broj = 1;
+
+            while (broj != 0)
+            {
+                Console.Write("Unesite prirodan broj: ");
+                broj = int.Parse(Console.ReadLine());
+                suma += broj;
+            }
+            Console.WriteLine("Suma unašenih brojeva je {0}", suma);
+            Console.ReadKey();
+        }
+    }
+}

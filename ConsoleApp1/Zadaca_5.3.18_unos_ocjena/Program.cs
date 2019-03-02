b44598608d1365a5a5b4084b1e0e80c863d6e17a
rihard11 <rihardla@gmail.com>
2019-03-02 17:23:06 +0100

    dodan Zadaca_5.3.18_unos_ocjena

---
+++ ConsoleApp1/Zadaca_5.3.18_unos_ocjena/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca_5._3._18_unos_ocjena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ocjena = 0;
+            bool unosObavljen = false;
+
+            while (!unosObavljen)
+            {
+                Console.WriteLine("Unesite ocjenu: ");
+                ocjena = int.Parse(Console.ReadLine());
+
+                if (ocjena == 1 || ocjena == 2 || ocjena == 3 || ocjena == 4 || ocjena == 5)
+                {
+                    unosObavljen = true;
+                }
+                else
+                {
+                    Console.WriteLine("Pogresan unos.");
+                }
+            }
+            Console.WriteLine("Ocjena je unesena. Unjeli ste {0} kao ocjenu.", ocjena);
+            Console.Read();
+        }
+    }
+}
+

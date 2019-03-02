670dc9269aaee2781fb7ee442f4930ffe652f224
rihard11 <rihardla@gmail.com>
2019-03-02 15:46:22 +0100

    dodan Zadaca_5.3.14_neparni_brojevi

---
+++ ConsoleApp1/Zadaca_5.3.14_neparni_brojevi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca_5._3._14_neparni_brojevi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}

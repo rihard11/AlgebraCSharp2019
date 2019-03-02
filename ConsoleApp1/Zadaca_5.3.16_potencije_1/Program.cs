7fde33dd3d3b6e9fa97a9538d552a725006e01b2
rihard11 <rihardla@gmail.com>
2019-03-02 16:02:58 +0100

    dodan Zadaca_5.3.16_potencije_1

---
+++ ConsoleApp1/Zadaca_5.3.16_potencije_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca_5._3._16_potencije_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Potencije broja 2 manje od 1000 su:");
+            for (int i = 1; i < 1000; i*=2)
+            {
+                Console.WriteLine("{0}", i);
+            }
+            Console.ReadKey();
+        }
+    }
+}

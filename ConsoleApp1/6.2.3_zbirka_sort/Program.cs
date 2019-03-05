a683887e69cb303e2d7912408e71728d9324d04f
programer10 <programer10@ucione.local>
2019-03-05 17:20:39 +0100

    dodan 6.2.3_zbirka_znamenke

---
+++ ConsoleApp1/6.2.3_zbirka_sort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2._3_zbirka_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listaBrojeva = new List<int>();
+            int broj = -1;
+
+            Console.WriteLine("Unesite brojeve, za kraj unesite 0");
+
+            while (broj != 0)
+            {
+                broj = int.Parse(Console.ReadLine());
+                listaBrojeva.Add(broj);
+            }
+            listaBrojeva.Sort();
+
+            foreach (int item in listaBrojeva)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}

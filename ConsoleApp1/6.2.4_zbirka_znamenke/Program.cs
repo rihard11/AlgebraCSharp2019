689da3910edbbcd59177a69a2c6419f41b5451e5
programer10 <programer10@ucione.local>
2019-03-05 17:32:39 +0100

    dodan 6.2.4_zbirka_znamenke

---
+++ ConsoleApp1/6.2.4_zbirka_znamenke/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2._4_zbirka_znamenke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite prirodni broj");
+            int broj = int.Parse(Console.ReadLine());
+
+            ArrayList arr = new ArrayList();
+
+            while (broj > 0)
+            {
+                arr.Add(broj % 10);
+                broj /= 10;
+            }
+
+            Console.WriteLine("Ispis znamenki naopako: ");
+            for (int i = 0; i < arr.Count; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}

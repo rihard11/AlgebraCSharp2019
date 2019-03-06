fb313311000862aaecb76b7d7a9d5349e430d901
rihard11 <rihardla@gmail.com>
2019-03-06 13:10:22 +0100

    zadaca_7.2.3_ks_u_kw_obrnuto

---
+++ Zadaca_funkcije/7.2.3_ks_u_kw_obrnuto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2._3_ks_u_kw_obrnuto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite snagu u KW (kilovatima): ");
+            double kw = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Snaga izražena u konjskim snagama (KS) je {0} KS", KWtoKS(kw));
+            Console.ReadKey();
+        }
+
+        static double KWtoKS(double kw)
+        {
+            return Math.Round(kw / 0.736);
+        }
+    }
+}

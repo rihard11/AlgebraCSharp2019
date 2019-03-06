2b64a3518308adee70a904e172f58438951c802f
rihard11 <rihardla@gmail.com>
2019-03-06 10:05:50 +0100

    zadaca_7.2.4_celzijus_u_fahrenheit

---
+++ Zadaca_funkcije/7.2.4_celzij_u_fahrenheit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2._4_celzij_u_fahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite stupnjeve u Celzijusu (°C): ");
+            double celzijus = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} °C = {1} °F", celzijus, CtoF(celzijus));
+            Console.ReadKey();
+        }
+
+        static double CtoF(double celzijus)
+        {
+            return celzijus * 9 / 5 + 32;
+        }
+    }
+}

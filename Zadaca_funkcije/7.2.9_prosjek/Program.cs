985c5405c482417f957845b4c67bcbd4619e3950
rihard11 <rihardla@gmail.com>
2019-03-06 11:20:21 +0100

    zadaca_7.2.9_prosjek

---
+++ Zadaca_funkcije/7.2.9_prosjek/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2._9_prosjek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite prosječnu ocjenu: ");
+            double broj = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Prosjek je {0}.", Prosjek(broj));
+            Console.ReadKey();
+        }
+
+        static string Prosjek(double broj)
+        {
+            string prosjek = "";
+
+            if (broj < 2)
+            {
+                prosjek = "nedovoljan";
+
+            }
+            else if (broj < 2.5)
+            {
+                prosjek = "dovoljan";
+            }
+            else if (broj < 3.5)
+            {
+                prosjek = "dobar";
+            }
+            else if (broj < 4.5)
+            {
+                prosjek = "vrlo dobar";
+            }
+            else
+            {
+                prosjek = "odličan";
+            }
+
+            return prosjek;
+        }
+    }
+}

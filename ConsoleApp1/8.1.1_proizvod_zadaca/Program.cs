717246acbddd20d73808004db57c9d8d559632c8
rihard11 <rihardla@gmail.com>
2019-03-07 11:58:53 +0100

    8.1.1_proizvod_zadaca

---
+++ ConsoleApp1/8.1.1_proizvod_zadaca/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1._1_proizvod_zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proizvod p1 = new Proizvod();
+
+            Console.Write("Unesite naziv proizvoda: ");
+            p1.naziv = Console.ReadLine();
+
+            Console.Write("Unesite cijenu proizvoda: ");
+            p1.cijena = double.Parse(Console.ReadLine());
+
+            Console.Write("Unesite marzu proizvoda: ");
+            p1.marza = double.Parse(Console.ReadLine());
+
+            Console.Write("Unesite porez na proizvod: ");
+            p1.porez = double.Parse(Console.ReadLine());
+
+            // Ispis
+            Console.WriteLine("Maloprodajna cijena za proizvod {0} je {1}", p1.naziv, p1.MPC());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/8.1.1_proizvod_zadaca/Proizvod.cs
@@ -0,0 +1,16 @@
+﻿namespace _8._1._1_proizvod_zadaca
+{
+    internal class Proizvod
+    {
+        public string naziv= "";
+        public double cijena = 0;
+        public double marza = 0;
+        public double porez = 0;
+
+        // Metoda
+        public double MPC()
+        {
+            return cijena + marza + porez;
+        }
+    }
+}

95294be206bdc40bd15e9d05c63f355f88dadc9d
rihard11 <rihardla@gmail.com>
2019-03-08 12:59:10 +0100

    8.2.2_static_istanca_zadaca

---
+++ ConsoleApp1/8.2.2_static_instanca_zadaca/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._2._2_static_instanca_zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite prirodan broj: ");
+            int broj = int.Parse(Console.ReadLine());
+
+            if (Static_Instanca.Prost(broj))
+            {
+                Console.WriteLine("Broj je prost.");
+            }
+            else
+            {
+                Console.WriteLine("Broj NIJE prost.");
+            }
+
+            Static_Instanca SI = new Static_Instanca();
+            if (SI.Savrsen(broj))
+            {
+                Console.WriteLine("Broj je savršen");
+            }
+            else
+            {
+                Console.WriteLine("Broj NIJE savršen");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/8.2.2_static_instanca_zadaca/Static_Instanca.cs
@@ -0,0 +1,49 @@
+﻿namespace _8._2._2_static_instanca_zadaca
+{
+    internal class Static_Instanca
+    {
+        #region STAIC metoda
+
+        public static bool Prost(int broj)
+        {
+            bool prost = true;
+
+            for (int i = 2; i < broj; i++)
+            {
+                if (broj % i == 0)
+                {
+                    prost = false;
+                    break;
+                }
+            }
+            return prost;
+        }
+        #endregion
+
+        #region Metoda instance
+
+        public bool Savrsen(int broj)
+        {
+            int zbrojDjelitelja = 0;
+
+            for (int i = 1; i < broj; i++)
+            {
+                if (broj % i == 0)
+                {
+                    zbrojDjelitelja = zbrojDjelitelja + i;
+                }
+            }
+
+            if (broj == zbrojDjelitelja)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        #endregion
+    }
+}

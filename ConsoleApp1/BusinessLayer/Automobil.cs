fde7bb55efe3c43077fc2c7144b547b13a8b7143
programer10 <programer10@ucione.local>
2019-03-13 16:39:01 +0100

    class_library

---
+++ ConsoleApp1/BusinessLayer/Automobil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Automobil
+    {
+        public string MarkaAutomobila = "";
+        public double KS = 0;
+        public double osnovnaCijena = 0;
+
+        public double IznosPoreza()
+        {
+            double postotak = 0;
+
+            if (KS < 50)
+            {
+                postotak = 0.05;
+            }
+            else if (KS > 50 && KS < 150)
+            {
+                postotak = 0.1;
+            }
+            else
+            {
+                postotak = 0.15;
+            }
+            return osnovnaCijena * postotak;
+        }
+
+        public double UkupnaCijena()
+        {
+            return osnovnaCijena + IznosPoreza();
+        }
+    }
+}

2c0f20bb7ab428d9bc585e8956b152c2507a5958
programer10 <programer10@ucione.local>
2019-03-13 18:50:20 +0100

    9.1_Rad_s_textom

---
+++ ConsoleApp1/9.1.1_17_manipulacija/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1._1_17_manipulacija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string recenica = "Danas je suncan dan, zato vam Dobar dan!";
+            string rijec = "dan";
+            char slovo = 'n';
+
+            int brojac = 0;
+            for (int i = 0; i < recenica.Length; i++)
+            {
+                if (recenica[i] == slovo)
+                {
+                    brojac++;
+                }
+            }
+            Console.WriteLine("9.1.1 Znak u rijeci");
+            Console.WriteLine("Znak {0} pojavljuje se u rijeci '{1}' {2} puta", slovo, rijec, brojac);
+
+            // recenica = recenica.ToLower();
+            string[] nizrijeci = recenica.Split(' ', ',', '!');
+            brojac = 0;
+            for (int i = 0; i < nizrijeci.Length; i++)
+            {
+                if (nizrijeci[i].ToLower() == rijec.ToLower())
+                {
+                    brojac++;
+                }
+            }
+
+            Console.WriteLine("9.1.2 Rijec u recenici");
+            Console.WriteLine("Rijec {0} pojavljuje se u recenici '{1}' {2} puta", rijec, recenica, brojac);
+
+
+
+            Console.WriteLine("9.1.3 Rijeci u novi red");
+            for (int i = 0; i < nizrijeci[i].Length; i++)
+            {
+                Console.WriteLine(nizrijeci[i]);
+            }
+
+            nizrijeci = recenica.Split(' ');
+            Console.WriteLine("9.1.4 Brojac rijeci u recenici");
+            Console.WriteLine("Recenica '{0}' ima {1} rijeci", recenica, nizrijeci.Length);
+
+            Console.ReadKey();
+        }
+    }
+}

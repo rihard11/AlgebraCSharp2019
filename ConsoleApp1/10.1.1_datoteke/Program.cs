8fc5bd5ca1253f22c5a27295ffbe6d028a777498
programer10 <programer10@ucione.local>
2019-03-14 16:50:37 +0100

    10.1.X_datoteke

---
+++ ConsoleApp1/10.1.1_datoteke/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._1._1_datoteke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upišite neki text: ");
+            string upis = Console.ReadLine();
+
+            // Kreiranje handle
+            FileStream fs1 = new FileStream("Proba.txt", FileMode.Create);
+            // Zapisivanje u datoteku
+            StreamWriter sw1 = new StreamWriter(fs1);
+
+            sw1.WriteLine(upis);
+
+            sw1.Flush();
+
+            sw1.Close();
+            fs1.Close();
+
+            // Čitanje iz datoteke
+            FileStream fs2 = new FileStream("Proba.txt", FileMode.Open);
+            StreamReader sr1 = new StreamReader(fs2);
+
+            string procitano = sr1.ReadToEnd();
+            Console.WriteLine("Ispis iz datoteke Proba.txt");
+            Console.WriteLine(procitano);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/10.1.2_primjer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._1._2_primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mapa u kojoj se izvršava naš program: ");
+            Console.WriteLine(Environment.CurrentDirectory);
+
+            Console.WriteLine("Postoji li Proba.txt u izvršnoj mapi: ");
+            // Path clasa
+            string mojaPutanja = Path.Combine(Environment.CurrentDirectory, "Proba.txt");
+
+            Console.WriteLine(File.Exists(mojaPutanja));
+
+            if (!Directory.Exists("Proba"))
+            {
+                Directory.CreateDirectory("Proba");
+            }
+
+            Console.WriteLine("Direktoriji na C: ");
+            string[] sDirs = Directory.GetDirectories(@"C:\");
+            foreach (var sDir in sDirs)
+            {
+                Console.WriteLine(sDir);
+            }
+
+            Console.WriteLine("Datoteke na C: ");
+            string[] sFiles = Directory.GetFiles(@"C:\");
+            foreach (var sFile in sFiles)
+            {
+                Console.WriteLine(Path.GetFileName(sFile));
+            }
+            Console.ReadKey();
+        }
+    }
+}

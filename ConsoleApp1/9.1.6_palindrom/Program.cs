fc06ce42e623f796778b50518d0271895f454b13
rihard11 <rihardla@gmail.com>
2019-03-14 13:14:09 +0100

    9.1.6_palindrom

---
+++ ConsoleApp1/9.1.6_palindrom/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1._6_palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite tekst\n");
+            string tekst = Console.ReadLine();
+
+            tekst = tekst.Replace(" ", "");
+
+            string obrnuto = "";
+            for (int i = tekst.Length - 1; i >= 0; i--)
+            {
+                obrnuto += tekst[i];
+            }
+            if (tekst == obrnuto)
+            {
+                Console.WriteLine("Ovo je PALINDROM.");
+            }
+            else
+            {
+                Console.WriteLine("To nije PALINDROM.");
+            }
+            Console.ReadKey();
+        }
+    }
+}

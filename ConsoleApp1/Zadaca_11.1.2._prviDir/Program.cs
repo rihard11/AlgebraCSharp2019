a3a28abdafbad490e90bfa550feeebb084e19582
rihard11 <rihardla@gmail.com>
2019-03-15 13:49:53 +0100

    Zadaca_11.1.2._prviDir

---
+++ ConsoleApp1/Zadaca_11.1.2._prviDir/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca_11._1._2._prviDir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upišite putanju: ");
+
+            string putanja = Console.ReadLine();
+
+            if (!Directory.Exists(putanja))
+            {
+                Console.WriteLine("Navedena putanja ne postoji.");
+            }
+            else
+            {
+                string dir = (from di in Directory.GetDirectories(putanja)
+                              orderby Path.GetDirectoryName(di) descending
+                              select di).FirstOrDefault();
+                if (!string.IsNullOrEmpty(dir))
+                {
+                    Console.WriteLine("Pronađeni direktorij: " + Path.GetFileName(dir));
+                }
+                else
+                {
+                    Console.WriteLine("Nije pronađen niti jedan direktorij.");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Zadaca_11.1.2._prviDir/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Zadaca_11.1.2._prviDir")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zadaca_11.1.2._prviDir")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4741a066-e026-4045-be6b-b8a7d09ec09d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

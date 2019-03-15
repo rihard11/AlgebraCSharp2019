02e8f665620e9cadeecc587637675cd95e0537f4
rihard11 <rihardla@gmail.com>
2019-03-15 13:45:05 +0100

    Zadaca_11.1.1._imena

---
+++ ConsoleApp1/Zadaca_11.1.1._imena/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca_11._1._1._imena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> imena = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Upisite ime: ");
+                string ime = Console.ReadLine();
+                if (ime.ToLower() == "kraj")
+                {
+                    break;
+                }
+                else
+                {
+                    imena.Add(ime);
+                }
+            }
+            List<string> imenaSaSlovomA = (
+                from im in imena
+                where im.ToLower().Contains("a")
+                select im).ToList();
+
+            Console.WriteLine();
+            Console.Write("Imena koja sadrže slovo \"a\" su: ");
+
+            foreach (string ime in imenaSaSlovomA)
+            {
+                Console.Write(ime + ", ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
